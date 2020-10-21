3f5699d4431417b19e481189398aedab7227bba1
catd825 <catd825@gmail.com>
2020-10-21 12:11:50 -0400

    updated about page and some css

--- src/Containers/AboutContainer.js
+++ src/Containers/AboutContainer.js
@@ -16,23 +16,30 @@ class About extends React.Component {
                 <br/>
                 <h3>ABOUT CATHY</h3>
                 <br/>
-                    <p>Full stack web developer experienced in Javascript React and Ruby on Rails. Past experience as operational finance manager with 9+ years at advertising agencies.</p> 
-                
-                    <p>Experienced in revenue, forecasting, contract negotiation, resource and budget management. 
+                    <p>Brooklyn-based full stack web developer experienced in all technologies below, including React.js and Ruby on Rails.</p> 
+                    <br/>
+                    <p>
+                        <SiReact size={30}/>
+                        &nbsp;&nbsp;&nbsp;&nbsp;
+                        <SiJavascript size={30}/>
+                        &nbsp;&nbsp;&nbsp;&nbsp;
+                        <SiRuby size={30}/>
+                        &nbsp;&nbsp;&nbsp;&nbsp;
+                        <SiRails size={30}/>
+                        &nbsp;&nbsp;&nbsp;&nbsp;
+                        <SiHtml5 size={30}/>
+                        &nbsp;&nbsp;&nbsp;&nbsp;
+                        <SiCss3 size={30}/>
+                    </p>
+
+                    <br/>
+                    
+                    <p>Past experience as operational finance manager with 9+ years at advertising agencies.  Strong communication skills.  Experienced in revenue, forecasting, contract negotiation, resource and budget management. 
                         Skilled in cross functional collaboration for both client accounts and internal agency operating budgets. </p>
-                <p>
-                    <SiReact size={30}/>
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-                    <SiJavascript size={30}/>
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-                    <SiRuby size={30}/>
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-                    <SiRails size={30}/>
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-                    <SiHtml5 size={30}/>
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-                    <SiCss3 size={30}/>
-                </p>
+                    <p>
+                        Interested in a role where I can continue to build on my programming skills, as well as transfer my previous professional experience.  Please feel free to reach out via email or LinkedIn at the bottom of this page.
+                    </p>
+                        
             </div>
         )
     }
--- src/Containers/WorkContainer.js
+++ src/Containers/WorkContainer.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { SiReact } from "react-icons/si";
+import { SiRails } from "react-icons/si";
 
 class WorkContainer extends React.Component {
     render() {
         return (
             <div className="container">
-                <h3>Projects</h3>
+                <h3>PROJECTS</h3>
                 <table>
                     <tbody>
                         <tr>
@@ -12,6 +14,9 @@ class WorkContainer extends React.Component {
                                 <h5>budgetBuddy</h5>
                                     A simple way to create and modify monthly budgets and transactions.  This is a capstone project for the Flatiron School.
                                 <br/>
+                                <p><b>Technologies Used:</b>  &nbsp;&nbsp;
+                                <SiReact size={30}/>&nbsp;&nbsp;<SiRails size={30}/>
+                                </p>
                                     <b><a href="https://youtu.be/xrYvw2IYEmI" target="_blank" rel="noopener noreferrer">Demo</a> | <a href="https://github.com/catd825/budgetBuddy_frontend" target="_blank" rel="noopener noreferrer">GitHub</a></b>
                             </td>
                             <td>
@@ -26,6 +31,9 @@ class WorkContainer extends React.Component {
                                 <h5>feelingsOverflow</h5>
                                 Forum-based app in which users post, comment, and vote on mental health questions and concerns.
                                 <br/>
+                                <p><b>Technologies Used:</b>  &nbsp;&nbsp;
+                                <SiReact size={30}/>&nbsp;&nbsp;<SiRails size={30}/>
+                                </p>
                                 <b><a href="https://j.gifs.com/L7oDK4.gif" target="_blank" rel="noopener noreferrer">Demo</a> | <a target="_blank" rel="noopener noreferrer" href="https://github.com/catd825/feelingsOverflow-front-end">Github</a></b>
                             </td>
                         </tr>
@@ -34,6 +42,9 @@ class WorkContainer extends React.Component {
                                 <h5>Fluffy Tail</h5>
                                 Fluffy tail is a Ruby on Rails app that allows you to browse and favorite pets to adopt in the NY Metro area. 
                                 <br/>
+                                <p><b>Technologies Used:</b>  &nbsp;&nbsp;
+                                <SiRails size={30}/>
+                                </p>
                                     <b><a href="https://youtu.be/scLpLe3zIxI" target="_blank" rel="noopener noreferrer">Demo</a> | <a href="https://github.com/catd825/fluffy_tail" target="_blank" rel="noopener noreferrer">GitHub</a></b>
                             </td>
                             <td><img height="60%" src="https://j.gifs.com/NLqWyN.gif" alt="fluffy tail"></img></td>
