167f6db07d35d39bda578e1babb1a48d20dbc763
catd825 <catd825@gmail.com>
2020-10-29 12:27:20 -0400

    updates

--- src/Containers/AboutContainer.js
+++ src/Containers/AboutContainer.js
@@ -1,10 +1,5 @@
 import React from 'react'
-import { SiReact } from "react-icons/si";
-import { SiJavascript } from "react-icons/si";
-import { SiRuby } from "react-icons/si";
-import { SiRails } from "react-icons/si";
-import { SiHtml5 } from "react-icons/si";
-import { SiCss3 } from "react-icons/si";
+
 import { NavLink } from "reactstrap"
 import { Link } from "react-router-dom"
 
@@ -13,33 +8,22 @@ import { Link } from "react-router-dom"
 class About extends React.Component {
     render(){
         return(
-            <div className="container" id="about">
+            <div className="container" id="about" style={{width: "60%"}}>
                 <h3>ABOUT</h3>
+                <br/>
+                <img width="150px" src={require('../images/cathy.png')} />
                 <br/><br/>
-                <img width="200px" src={require('../images/cathy.png')} />
-                <br/><br/>
-                    <p>Cathy is a Brooklyn-based full stack web developer experienced in all technologies below, including React.js and Ruby on Rails.</p> 
-                    <br/>
-                    <p>
-                        <SiReact size={30}/>
-                        &nbsp;&nbsp;&nbsp;&nbsp;
-                        <SiJavascript size={30}/>
-                        &nbsp;&nbsp;&nbsp;&nbsp;
-                        <SiRuby size={30}/>
-                        &nbsp;&nbsp;&nbsp;&nbsp;
-                        <SiRails size={30}/>
-                        &nbsp;&nbsp;&nbsp;&nbsp;
-                        <SiHtml5 size={30}/>
-                        &nbsp;&nbsp;&nbsp;&nbsp;
-                        <SiCss3 size={30}/>
-                    </p>
-
-                    <br/>
+                    <p className="left">
+                        Hi!  I'm Cathy.  I am a full stack web developer based in Brooklyn NY, primarily experienced React.js and Ruby on Rails.
+                    </p> 
                     
-                    <p>Past experience as operational finance manager with 9+ years at advertising agencies.  Strong communication skills.  Experienced in revenue, forecasting, contract negotiation, resource and budget management. 
-                        Skilled in cross functional collaboration for both client accounts and internal agency operating budgets. </p>
-                    <p>
-                        Interested in a role where I can continue to build on my programming skills, as well as transfer my previous professional experience.  Please feel free to reach out via email or LinkedIn at the bottom of this page.
+                    <p className="left">
+                        I completed the Software Engineering Bootcamp at the Flatiron  School in Fall of 2020.  Prior to programming, I spent the last ~10 years as an operational finance manager at advertising agencies.  I'm experienced in revenue, forecasting, contract negotiation, resource and budget management. 
+                        I'm skilled in cross functional collaboration for both client accounts and internal agency operating budgets. 
+                    </p>
+                    
+                    <p className="left">
+                        I'm interested in a role where I can continue to build on my programming skills, as well as transfer my previous professional experience.  Please feel free to reach out via email or LinkedIn at the bottom of this page.
                     </p>
                     <br/>
                     <NavLink to="/home" tag={Link}>return to main</NavLink>        
