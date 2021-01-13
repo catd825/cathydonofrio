06fb36b725ccc208e0bfa7ee66dc753a62e59844
catd825 <catd825@gmail.com>
2021-01-13 11:00:34 -0500

    updated contact

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import { SiLinkedin } from "react-icons/si";
-import { SiGithub } from "react-icons/si";
-import { SiGmail } from "react-icons/si";
+import { SiLinkedin, SiGithub, SiGmail, SiMedium } from "react-icons/si";
 
 
 
 const Footer = () => {
     return(
         <div className="footer">
-            Contact me:  &nbsp;&nbsp;&nbsp;
+            Find me:  &nbsp;&nbsp;&nbsp;
             <a href="http://www.linkedin.com/in/cadonofrio" target="_blank" rel="noopener noreferrer"><SiLinkedin size={20}/></a>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <a href="http://www.github.com/catd825" target="_blank" rel="noopener noreferrer"><SiGithub size={20}/></a>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <a href="mailto:catd825@gmail.com"><SiGmail size={20}/></a> &nbsp;&nbsp;
-            {/* | &nbsp;&nbsp;Photo by Cathy D'Onofrio */}
+            <a href="mailto:catd825@gmail.com"><SiGmail size={20}/></a>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <a href="http://catd825.medium.com" target="_blank" rel="noopener noreferrer"><SiMedium size={20}/></a>
         </div>
     )
 }
